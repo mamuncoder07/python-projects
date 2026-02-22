5a607aa0c065d5856079ea35a47d6d14fa61384d
Mamun Reja <mamun20030318reja@gmail.com>
2026-02-22 21:39:21 +0530

    Add password generator functionality

--- Password_Gennerator/main.py
+++ Password_Gennerator/main.py
@@ -1,1 +1,13 @@
+import random
+import string
 
+# All characters (letters + numbers + symbols)
+characters = string.ascii_letters + string.digits + string.punctuation
+
+# Password length
+length = 16
+
+# Generate password
+password = "".join(random.choice(characters) for i in range(length))
+
+print("Generated Password:", password)
